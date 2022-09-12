3aa8491cdcc20eaa973190de170d4897f79abf3d
Jenna Tuomikoski <jenna.tuomikoski@edu.keuda.fi>
2022-09-12 11:29:33 +0300

    1209

---
+++ JavaScript/javascript-perusteet.js
@@ -0,0 +1,31 @@
+
+  function laskut() {
+      var luku1, luku2, summa, erotus, osamaara, tulo;
+      luku1 = 8;
+      luku2 = 2;
+      summa = luku1 + luku2;
+      erotus = luku1 - luku2;
+      osamaara = luku1 / luku2;
+      tulo = luku1 * luku2;
+
+
+    console.log('Summa on ' + summa);
+    console.log('Erotus on ' + erotus);
+    console.log('Osamäärä on ' + osamaara);
+    console.log('Tulo on ' + tulo);
+  }
+function liitaSanat() {
+  var t1, t2, t3;
+  t1 = "Jenna "
+  t2 = "opiskelee "
+  t3 = "JavaScriptiä"
+  console.log(t1 + t2 + t3);
+}
+function attribuutti(nimi) {
+  var t1 = " on ";
+  var t2 = "hyvä opiskelija";
+  console.log(nimi + t1 + t2);
+}
+function laskeYhteen(luku1, luku2) {
+  console.log(luku1 + luku2);
+}
